6e5324340d2a02a9df552be1b3dc8f6a2bbf65bb
mgarin <mgarin@alee.com>
2016-02-03 19:39:01 +0300

    Separator - AbstractSeparatorPainter.java - Defended against empty lines - Convenient separator default transparency

--- modules/ui/src/com/alee/managers/style/skin/web/AbstractSeparatorPainter.java
+++ modules/ui/src/com/alee/managers/style/skin/web/AbstractSeparatorPainter.java
@@ -24,58 +24,78 @@ public abstract class AbstractSeparatorPainter<E extends JSeparator, U extends S
      */
     protected SeparatorLines lines;
 
+    /**
+     * Returns separator lines count.
+     *
+     * @return separator lines count
+     */
+    protected int getLinesCount ()
+    {
+        return lines != null && lines.getLines () != null ? lines.getLines ().size () : 0;
+    }
+
+    @Override
+    public Boolean isOpaque ()
+    {
+        return false;
+    }
+
     @Override
     public void paint ( final Graphics2D g2d, final Rectangle bounds, final E c, final U ui )
     {
-        final Object aa = GraphicsUtils.setupAntialias ( g2d );
-
-        // General settings
-        final Insets insets = c.getInsets ();
-        final int x = insets.left;
-        final int y = insets.top;
-        final int width = c.getWidth () - insets.left - insets.right;
-        final int height = c.getHeight () - insets.top - insets.bottom;
-        final boolean vertical = c.getOrientation () == WebSeparator.VERTICAL;
-
-        // Painting each separator line
-        for ( int i = 0; i < lines.getLines ().size (); i++ )
+        if ( getLinesCount () > 0 )
         {
-            // Current line
-            final SeparatorLine line = lines.getLines ().get ( i );
+            final Object aa = GraphicsUtils.setupAntialias ( g2d );
 
-            // Calculating line coordinates
-            final int x1;
-            final int y1;
-            final int x2;
-            final int y2;
-            if ( vertical )
+            // General settings
+            final Insets insets = c.getInsets ();
+            final int x = insets.left;
+            final int y = insets.top;
+            final int width = c.getWidth () - insets.left - insets.right;
+            final int height = c.getHeight () - insets.top - insets.bottom;
+            final boolean vertical = c.getOrientation () == WebSeparator.VERTICAL;
+
+            // Painting each separator line
+            for ( int i = 0; i < lines.getLines ().size (); i++ )
             {
-                x1 = x2 = ltr ? x + i : x + width - i - 1;
-                y1 = y;
-                y2 = y + height - 1;
-            }
-            else
-            {
-                x1 = x;
-                x2 = x + width - 1;
-                y1 = y2 = y + i;
+                // Current line
+                final SeparatorLine line = lines.getLines ().get ( i );
+
+                // Calculating line coordinates
+                final int x1;
+                final int y1;
+                final int x2;
+                final int y2;
+                if ( vertical )
+                {
+                    x1 = x2 = ltr ? x + i : x + width - i - 1;
+                    y1 = y;
+                    y2 = y + height - 1;
+                }
+                else
+                {
+                    x1 = x;
+                    x2 = x + width - 1;
+                    y1 = y2 = y + i;
+                }
+
+                // Painting line
+                final Stroke stroke = GraphicsUtils.setupStroke ( g2d, line.getStroke (), line.getStroke () != null );
+                final Paint op = GraphicsUtils.setupPaint ( g2d, line.getPaint ( x1, y1, x2, y2 ) );
+                g2d.drawLine ( x1, y1, x2, y2 );
+                GraphicsUtils.restorePaint ( g2d, op );
+                GraphicsUtils.restoreStroke ( g2d, stroke, line.getStroke () != null );
             }
 
-            // Painting line
-            final Stroke stroke = GraphicsUtils.setupStroke ( g2d, line.getStroke (), line.getStroke () != null );
-            g2d.setPaint ( line.getPaint ( x1, y1, x2, y2 ) );
-            g2d.drawLine ( x1, y1, x2, y2 );
-            GraphicsUtils.restoreStroke ( g2d, stroke, line.getStroke () != null );
+            GraphicsUtils.restoreAntialias ( g2d, aa );
         }
-
-        GraphicsUtils.restoreAntialias ( g2d, aa );
     }
 
     @Override
     public Dimension getPreferredSize ()
     {
         final Insets insets = component.getInsets ();
-        final int lines = this.lines.getLines ().size ();
+        final int lines = getLinesCount ();
         return component.getOrientation () == WebSeparator.VERTICAL ?
                 new Dimension ( insets.left + lines + insets.right, insets.top + insets.bottom ) :
                 new Dimension ( insets.left + insets.right, insets.top + lines + insets.bottom );
